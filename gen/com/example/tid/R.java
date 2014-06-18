756036eab846cade696e428a4c6f0f3db7d982bf
Rudy Dewantho Yudha <rudy.dewantho@gmail.com>
2014-06-18 12:04:09 +0700

    new 2

--- gen/com/example/tid/R.java
+++ gen/com/example/tid/R.java
@@ -11,10 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int sierra_icon=0x7f020002;
+    }
+    public static final class id {
+        public static final int Confirmation=0x7f060002;
+        public static final int RegCardConfirmation=0x7f060003;
+        public static final int btnMaintenance=0x7f060001;
+        public static final int btnNo=0x7f060005;
+        public static final int btnUnrege=0x7f060007;
+        public static final int btnYes=0x7f060004;
+        public static final int titleMaintenance=0x7f060000;
+        public static final int titleUnregister=0x7f060006;
+    }
+    public static final class layout {
+        public static final int button_green=0x7f030000;
+        public static final int button_red=0x7f030001;
+        public static final int maintenance=0x7f030002;
+        public static final int unreg_confirm=0x7f030003;
+        public static final int unregister=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,10 +57,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
